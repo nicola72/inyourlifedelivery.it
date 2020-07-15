9aa699f18b6d5c1c3d7a36e1ad3044a7901f2022
Nicola <nicola.tamburini@fjstudio.com>
2020-07-15 16:46:46 +0200

    commit

--- resources/views/cms/dashboard/index.blade.php
+++ resources/views/cms/dashboard/index.blade.php
@@ -1,1 +1,279 @@
 @extends('layouts.cms')
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox">
+                    <div class="ibox-title">
+                        <h2>Video Tutorial</h2>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>Accedi la tuo pannello</h3>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Login
+                                    </div>
+                                    <div class="panel-body">
+                                        Per effettuare l’accesso al tuo pannello di controllo
+                                        basterà collegarti al tuo sito web ***URL*** da Pc o da
+                                        smartphone ed accedere come nel video seguente.
+                                        <br><br>
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/o40KHmyZGbI?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>Configura il tuo negozio</h3>
+                        Durante la configurazione del tuo negozio potrai, tramite il pannello di impostazioni,
+                        modificare ogni aspetto della tua vendita online. Questi brevi video tutorial
+                        ti mostreranno come modificare gli aspetti più importanti della tua attività.
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Inserisci il tuo logo
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/Pkh8mRMLZPw?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Scegli i giorni di apertura
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/X5Do-4Q2uIM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Scegli i tuoi orari di apertura
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/lLCi-IJw_wY?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Scegli i comuni di consegna
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/o03KdYwttj0?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>Riempi il tuo menù</h3>
+                        Dopo aver configurato il tuo negozio non ti resta che riempire il tuo menù.
+                        Segui la seguente guida interattiva per poter riempire correttamente il tuo negozio.
+                    </div>
+                    <div class="ibox-title">
+                        <h3>PIZZERIA</h3>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        A - Crea gli ingredienti
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/3EztYdRGJjQ?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        B – Crea le categorie
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/DhL-Nc9tgkU?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        C - Crea le varianti delle tue pizze
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/UDLmFH8bA4c?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        D – Crea le tue pizze
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/tgVm3TcYYoI?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>GELATERIA</h3>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        A - Crea i gusti dei tuoi gelato
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/7s83uFcfd8s?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        B – Crea le categorie
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/DhL-Nc9tgkU?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        C – Crea le varianti dei tuoi gelati
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/UDLmFH8bA4c?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        D – Crea i tuoi gelati
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/J89f4t7YqQk?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>RISTORANTE</h3>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        A - Crea le categorie
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/DhL-Nc9tgkU?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        B – Crea le varianti dei tuoi piatti
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/UDLmFH8bA4c?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        C - Crea i tuoi piatti
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/mPxNelFdmIg?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-title">
+                        <h3>Ordini</h3>
+                        Dopo aver completato il tuo negozio, non dovrai far altro che controllare l’arrivo dei tuoi
+                        ordini dalla pagina specifica.
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row mb-2 pb-2 border-bottom">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Controlla i tuoi ordini
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="iframe-video">
+                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/JJDlrQpITWs?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('js_script')
+
+@stop
